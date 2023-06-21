b278ba13758b1393f904956f83dc226030908bbc
iis istiqomah <iisistiqomah20@gmail.com>
2023-06-21 17:48:36 +0700

    update index.js dan url koneksi di mongo.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import r_products from "./src/routers/products.js";
 
 const PORT = 4000
 const app = express();
@@ -7,5 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ limit: "5mb", extended: true}));
+
+app.use("/api/v1/", r_products);
 
 app.listen(PORT, () => console.log("Server running on port", PORT));
