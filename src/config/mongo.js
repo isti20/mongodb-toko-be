0715b3b5ebef82a691b13d493d52f4a26cb59caa
iis istiqomah <iisistiqomah20@gmail.com>
2023-06-21 17:35:03 +0700

    membuat konfigurasi koneksi ke mongoDB

---
+++ src/config/mongo.js
@@ -0,0 +1,15 @@
+import { MongoClient } from "mongodb"
+
+const url = `mongodb://0.0.0.0:27017`;
+const connection = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+(async () => {
+    try {
+      await connection.connect();
+    } catch (error) {
+      console.log("MongoDB", error)
+    }
+})();
+
+const db = connection.db("db_toko");
+export default db;
