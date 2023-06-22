e531c019d0226dca0fc0ef70d5e1cb8f93898e68
iis istiqomah <iisistiqomah20@gmail.com>
2023-06-22 17:57:16 +0700

    Membuat controller dan route CRUD

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,5 +1,6 @@
 import Products from "../models/products.js";
 import messages from "../utils/messages.js";
+import { ObjectId } from "mongodb";
 
 const getProduct = async (req, res) => {
     try {
@@ -11,4 +12,63 @@ const getProduct = async (req, res) => {
     }
 };
 
-export { getProduct };
+const getProductByID = async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const _id = new ObjectId(id);
+        const data = await Products.findOne({ _id });
+
+        if(data) return messages(res, 200, "Detail Product", data);
+
+        messages(res, 404, "Product not found");
+    } catch (error) {
+        messages(res, 500, error?.messages || "Internal server error");
+    }
+};
+
+const deleteProduct = async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const _id = new ObjectId(id);
+        const data = await Products.deleteOne({ _id });
+
+        if(data) return messages(res, 200, "Delete product succes");
+
+        messages(res, 404, "Product not found");
+    } catch (error) {
+        messages(res, 500, error?.messages || "Internal server error");
+    }
+};
+
+const createProduct = async (req, res) => {
+    const { name, price, stock } = req.body;
+
+    try {
+        const newData = await Products.insertOne({name, price, stock });
+
+        messages(res, 201, "Create product success", newData);
+    } catch (error) {
+        messages(res, 500, error?.messages || "Internal server error");
+    }
+};
+
+const updateProduct = async (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+
+    try {
+        const _id = new ObjectId(id);
+        const product = await Products.findOne({ _id });
+
+        if(product) {
+            await Products.updateOne({ _id }, { $set: { ...data }});
+            messages(res, 200, "Update product success");
+        }
+    } catch (error) {
+        messages(res, 500, error?.messages || "Internal server error");
+    }
+};
+
+export { getProduct, getProductByID, deleteProduct, createProduct, updateProduct };
--- src/routers/products.js
+++ src/routers/products.js
@@ -1,8 +1,17 @@
 import express from "express";
-import { getProduct } from "../controllers/products.js";
+import { 
+    getProduct, 
+    getProductByID, 
+    deleteProduct, 
+    createProduct, 
+    updateProduct } from "../controllers/products.js";
 
 const Router = express.Router();
 
 Router.get("/products", getProduct);
+Router.get("/products/:id", getProductByID);
+Router.delete("/products/:id", deleteProduct);
+Router.post("/products", createProduct);
+Router.put("/products/:id", updateProduct);
 
 export default Router;
